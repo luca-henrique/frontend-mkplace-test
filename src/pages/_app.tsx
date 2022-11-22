b2b59f7213b0776527f443f7fb95d48057388a5a
Lucas Henrique <lukas.paes18@gmail.com>
2022-11-22 01:48:32 -0300

    feature: configurando projeto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import type {AppProps} from 'next/app';
 import Head from 'next/head';
 
+import 'bootstrap/dist/css/bootstrap.css';
+
 function MyApp({Component, pageProps}: AppProps) {
   return (
     <>
