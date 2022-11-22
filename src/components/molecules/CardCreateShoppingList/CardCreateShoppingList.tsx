71793eb7b1be81f39b85095bcabb805d455f8902
Lucas Henrique <lukas.paes18@gmail.com>
2022-11-22 12:00:50 -0300

    feature: card para criar lista de compras

---
+++ src/components/molecules/CardCreateShoppingList/CardCreateShoppingList.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Image from 'next/image';
+
+import {Container, Title} from '../..';
+import {ICONS} from '../../../assets';
+
+const {plus} = ICONS;
+
+export const CardCreateShoppingList = () => {
+  return (
+    <Container
+      borderColor='red-300'
+      direction='row'
+      alignItems='center'
+      padding='12px 14px'
+      borderRadius='10px'
+      width='100%'
+    >
+      <Container
+        direction='row'
+        alignItems='center'
+        justifyContent='center'
+        background='red-100'
+        height='48px'
+        width='48px'
+        borderRadius='10px'
+      >
+        <Image src={plus} alt='mkplace' />
+      </Container>
+      <Container margin='0px 0px 0px 12px'>
+        <Title>Criar uma lista de compras</Title>
+      </Container>
+    </Container>
+  );
+};
