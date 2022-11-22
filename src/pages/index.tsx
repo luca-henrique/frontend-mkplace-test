a55a83260c31b13bb4c4bbec19155f60812e3565
Lucas Henrique <lukas.paes18@gmail.com>
2022-11-22 11:42:45 -0300

    refactor: base page and create components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,56 +1,25 @@
 import Image from 'next/image';
-import styled from 'styled-components';
 
-import {Container, ShoppingList} from '../components';
-
-import {ICONS} from '../assets';
-
-const {plus} = ICONS;
+import {Container, ShoppingList, CardCreateShoppingList} from '../components';
 
 const Home = () => {
   return (
-    <div className='d-flex flex-column justify-content-center align-items-center col-12'>
+    <Container
+      direction='column'
+      justifyContent='center'
+      alignItems='center'
+      height='100vh'
+      padding='0 40px'
+    >
       <div className='mb-4'>
         <Image src='/logo-mkplace.png' alt='logo' width={290} height={55} />
       </div>
 
-      <Container height='40px' />
-
       <ShoppingList />
 
-      <Container
-        borderColor='red-300'
-        direction='row'
-        alignItems='center'
-        padding='12px 14px'
-        borderRadius='10px'
-        width='100%'
-      >
-        <Container
-          direction='row'
-          alignItems='center'
-          justifyContent='center'
-          background='red-100'
-          height='48px'
-          width='48px'
-          borderRadius='10px'
-        >
-          <Image src={plus} alt='mkplace' />
-        </Container>
-        <Container margin='0px 0px 0px 12px'>
-          <Title>Criar uma lista de compras</Title>
-        </Container>
-      </Container>
-    </div>
+      <CardCreateShoppingList />
+    </Container>
   );
 };
-
-export const Title = styled.h1`
-  font-style: normal;
-  font-weight: 700;
-  font-size: 14px;
-  line-height: 21px;
-  color: #000000;
-`;
 
 export default Home;
