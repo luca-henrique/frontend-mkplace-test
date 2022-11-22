84f56ea513b755c6828c34faa887e562fb3c4c20
Lucas Henrique <lukas.paes18@gmail.com>
2022-11-22 11:41:17 -0300

    feature: create typography component and variant title

---
+++ src/components/atoms/Typography/index.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 21px;
+  color: #000000;
+`;
