8252298ba9c12d05f5c3bad7bab30fdd688c1dd9
Lucas Henrique <lukas.paes18@gmail.com>
2022-11-22 15:55:50 -0300

    feature: create component Header

--- src/components/molecules/Header/Header.tsx
+++ src/components/molecules/Header/Header.tsx
@@ -2,22 +2,22 @@ import Image from 'next/image';
 import Link from 'next/link';
 import {ICONS} from '../../../assets';
 
+import {Title} from '../..';
+
 const {arrowLeft} = ICONS;
 
 interface IHeader {
   routeDescription: string;
 }
 
-export default function Header(props: IHeader) {
+export const Header = ({routeDescription}: IHeader) => {
   return (
-    <header className='d-flex col-12 align-items-center'>
-      <Image src='/mkplate-icon.png' alt='logo' width={80} height={80} />
-
+    <header className='d-flex'>
       <Link href={'/'}>
         <Image src={arrowLeft} alt='Voltar' />
       </Link>
 
-      <h1 className='p-0 m-0'>{props.routeDescription}</h1>
+      <Title style={{marginLeft: '16px'}}>{routeDescription}</Title>
     </header>
   );
-}
+};
