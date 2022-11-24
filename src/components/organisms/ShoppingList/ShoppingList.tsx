f4d2e8bf05165411ab8a19972e3a9318e85d0eb5
Lucas Henrique <lukas.paes18@gmail.com>
2022-11-24 01:52:02 -0300

    feature: criando custom hook

--- src/components/organisms/ShoppingList/ShoppingList.tsx
+++ src/components/organisms/ShoppingList/ShoppingList.tsx
@@ -5,34 +5,12 @@ import {ShoppingListItem} from '../../';
 
 const api = new ListShoppingService();
 
+import {useShoppingListData} from '../../../hook/useShoppingListData';
+
 export const ShoppingList = () => {
-  const [shoppingListData, setShoppingListData] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const {data, loading} = useShoppingListData();
 
-  useEffect(() => {
-    getList();
-  }, []);
-
-  const getList = () => {
-    api.getList().then((resp) => {
-      const dataList = resp.map((elem: any) => {
-        let listCategoria = new Array();
-        elem.products.forEach((e) => {
-          if (!listCategoria.includes(e.categoryTitle))
-            listCategoria.push(e.categoryTitle);
-        });
-        elem.qtdeCategoria = listCategoria.length;
-        elem.qtdeItens = elem.products.length;
-
-        return elem;
-      });
-
-      setShoppingListData(dataList);
-      setLoading(false);
-    });
-  };
-
-  const renderList = shoppingListData.map((shoppingListItem, index) => (
+  const renderList = data.map((shoppingListItem, index) => (
     <ShoppingListItem {...shoppingListItem} key={index} />
   ));
 
---
+++ src/hook/useShoppingListData.tsx
@@ -0,0 +1,35 @@
+import {useEffect, useState} from 'react';
+
+import {ListShoppingService} from '../service/ListShoppingService';
+
+const shoppingListService = new ListShoppingService();
+
+export const useShoppingListData = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getList();
+  }, []);
+
+  const getList = () => {
+    shoppingListService.getList().then((resp) => {
+      const dataList = resp.map((elem: any) => {
+        let listCategoria = new Array();
+        elem.products.forEach((e) => {
+          if (!listCategoria.includes(e.categoryTitle))
+            listCategoria.push(e.categoryTitle);
+        });
+        elem.qtdeCategoria = listCategoria.length;
+        elem.qtdeItens = elem.products.length;
+
+        return elem;
+      });
+
+      setData(dataList);
+      setLoading(false);
+    });
+  };
+
+  return {data, loading};
+};
